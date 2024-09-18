681d8a8ef644d9b223ec317d47376b8c1463bc27
nguyenddat <DatND.B23CC032@stu.ptit.edu.vn>
2024-09-18 11:34:39 +0700

    init git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [job, setJob] = useState('')
+  const [jobs,setJobs] = useState(() => {
+    const storageJobs = JSON.parse(localStorage.getItem('jobs'))
+    return storageJobs ?? []
+  }) 
+  console.log(job)
+
+  function handleSubmit() {
+    setJobs(prev => {
+      const newJobs = [...prev, job]
+      const jsonJobs = JSON.stringify(newJobs)
+      localStorage.setItem('jobs', jsonJobs)
+
+      return [...prev, job]
+    })
+    setJob('')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding: 32}}>
+      <h2>Todo List</h2>
+      <input type='text' placeholder='Job Content' value={job} onChange={e => setJob(e.target.value)}></input>
+      <button type='submit' onClick={handleSubmit}>Add</button>
+
+      <ul>
+        {jobs.map((job, index) => (
+          <li key={index}>{job}</li>
+        ))}
+      </ul>
     </div>
   );
 }
